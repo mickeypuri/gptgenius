fcda3ecb01d6ca66f3dcc5754a55d76c6e8f7dc0
Mickey Puri <mickeypuri@hotmail.com>
2024-12-14 10:52:21 +0000

    added providers

---
+++ app/providers.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+
+const Providers = ({children}) => {
+  return (
+    <>
+      <Toaster position="top-center">
+        {children}
+      </Toaster>
+    </>
+  )
+};
+
+export default Providers;
