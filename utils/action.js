87f8552e857283065ded400988fb69303a3476a2
Mickey Puri <mickeypuri@hotmail.com>
2024-12-17 17:56:11 +0000

    added Open AI and display of messages

---
+++ utils/action.js
@@ -0,0 +1,29 @@
+'use server';
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAPI_API_KEY,
+});
+
+export const generateChatResponse = async (chatMessages) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {role: "system", content: "you are a helpful assistant"},
+        ...chatMessages
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    //console.log(response.choices[0].message);
+    //console.log(response);
+  
+    return response.choices[0].message;
+  }
+  catch (error) {
+    //console.log(error);
+    return null;
+  }
+  
+
+}
