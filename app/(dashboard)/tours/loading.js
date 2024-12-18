941c69d35cac162f863daab6b3453fc18b584305
Mickey Puri <mickeypuri@hotmail.com>
2024-12-18 20:11:50 +0000

    added all tours

---
+++ app/(dashboard)/tours/loading.js
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <span className="loading loading-lg">
+      loading
+    </span>
+  )
+};
+
+export default loading;
--- app/(dashboard)/tours/page.js
+++ app/(dashboard)/tours/page.js
@@ -1,4 +1,20 @@
-const ToursPage = () => {
-  return <div>Tours Page</div>;
-};
-export default ToursPage;
+import ToursPage from '@/components/ToursPage';
+import { getAllTours } from '@/utils/action';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
+export default async function AllToursPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['tours',''],
+    queryFn: () => getAllTours(),
+  });
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ToursPage />
+    </HydrationBoundary>
+  );
+}
