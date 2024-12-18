a7d9fde9bbbb2036f6bbf8652e47eb2db542f0f4
Mickey Puri <mickeypuri@hotmail.com>
2024-12-18 21:56:43 +0000

    added single tour page

---
+++ app/(dashboard)/tours/[id]/page.js
@@ -0,0 +1,22 @@
+import TourInfo from "@/components/TourInfo";
+import { getSingleTour } from "@/utils/action";
+import { redirect } from "next/dist/server/api-utils";
+import Link from "next/link";
+
+const SingleTourPage = async ({params}) => {
+  const tour = await getSingleTour(params.id);
+  
+  if (!tour) {
+    redirect('/tours');
+  }
+  return (
+    <div>
+      <Link href="/tours" className="btn btn-secondary mb-12">
+        back to tours
+      </Link>
+      <TourInfo tour={tour} />
+    </div>
+  )
+};
+
+export default SingleTourPage;
