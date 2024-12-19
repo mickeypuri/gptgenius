d80886ec9af4f0ec13fc5ce704df8d6917b2528b
Mickey Puri <mickeypuri@hotmail.com>
2024-12-19 08:09:54 +0000

    added ai generated image

--- app/(dashboard)/tours/[id]/page.js
+++ app/(dashboard)/tours/[id]/page.js
@@ -1,7 +1,8 @@
 import TourInfo from "@/components/TourInfo";
-import { getSingleTour } from "@/utils/action";
+import { generateTourImage, getSingleTour } from "@/utils/action";
 import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
+import Image from "next/image";
 
 const SingleTourPage = async ({params}) => {
   const tour = await getSingleTour(params.id);
@@ -9,11 +10,26 @@ const SingleTourPage = async ({params}) => {
   if (!tour) {
     redirect('/tours');
   }
+
+  const tourImage = await generateTourImage(tour);
+
   return (
     <div>
       <Link href="/tours" className="btn btn-secondary mb-12">
         back to tours
       </Link>
+      {
+        tourImage && <div>
+        <Image
+          src={tourImage}
+          width={300}
+          height={300}
+          className='rounded-xl shadow-xl mb-16 h-96 w-96 object-cover'
+          alt={tour.title}
+          priority
+        />
+      </div>
+      }
       <TourInfo tour={tour} />
     </div>
   )
